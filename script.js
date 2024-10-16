745520a62ad356c9bf99faac147384dff87e96c0
Mcdaid101 <matthewmcdaid1@hotmail.com>
2024-10-16 22:03:24 +0100

    fonts updated along with year function for footer

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ if (day === 1 || day === 2) {
 } else {
     document.getElementById("open-closed").innerHTML = "OPEN TODAY 8AM - 8PM"
 }
+
+const y = new Date();
+let year = y.getFullYear();
+
+document.getElementById("year").innerHTML = `${year}`;
