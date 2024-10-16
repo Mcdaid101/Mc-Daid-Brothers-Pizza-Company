25c9a8ee1516aa83c5e1c26ea440fa1d9df04161
Mcdaid101 <matthewmcdaid1@hotmail.com>
2024-10-16 21:45:22 +0100

    added fonts to home page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const d = new Date();
 let day = d.getDay();
 
 if (day === 1 || day === 2) {
-    document.getElementById("open-closed").innerHTML = "Sorry We're Closed Today"
+    document.getElementById("open-closed").innerHTML = "SORRY CLOSED TODAY"
 } else {
-    document.getElementById("open-closed").innerHTML = "We're open today from 8:00am to 8:30pm"
+    document.getElementById("open-closed").innerHTML = "OPEN TODAY 8AM - 8PM"
 }
